16b128ae5ccd2c69f7f0187658c3387cb3d9fb0c
Ilya Rogatkin <toch.in.go@googlemail.com>
2026-04-14 21:52:29 +0500

    add information about project in settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str = "Payment Service API"
+    VERSION: str = "1.0.0"
     DATABASE_URL: str = "postgresql+asyncpg://user:pass@localhost/db"
     BANK_API_URL: str = "https://api.examplebank.com"
     BANK_API_KEY: str = "your_bank_api_key"
     BANK_TIMEOUT: float = 5.0
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env",extra="ignore")
 
 settings = Settings()
