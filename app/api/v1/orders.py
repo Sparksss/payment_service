f8ffd993b3d6d913f734501bf903b8d30ef1a084
Ilya Rogatkin <toch.in.go@googlemail.com>
2026-04-14 21:53:59 +0500

    add orders api

--- app/api/v1/orders.py
+++ app/api/v1/orders.py
@@ -1,5 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.session import get_db
 from app.api.schemas.order import OrderRead
-from app.services
+
+
+router = APIRouter(prefix="/orders", tags=["orders"])
 
 @router.get("/{order_id}", response_model=OrderRead)
-async def read_order(order_id: int ,db: Async):
+async def read_order(order_id: int ,db: AsyncSession = Depends(get_db)):
+    db_order = await db.get(OrderRead, order_id)
+    if not db_order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+    return db_order
+
+@router.post("/{order_id}/pay", response_model=OrderRead)
+async def pay_order(order_id: int, db: AsyncSession = Depends(get_db)):
