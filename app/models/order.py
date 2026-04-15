e5319c1d112770fe394b09209e61426a03394b67
Ilya Rogatkin <toch.in.go@googlemail.com>
2026-04-15 20:28:53 +0500

    change to pydantic 2 style model order

--- app/models/order.py
+++ app/models/order.py
@@ -1,20 +1,26 @@
 import enum
 from decimal import Decimal
-from typing 
+from typing import List, TYPE_CHECKING
 
-from sqlalchemy import Column, Integer, Numeric, Enum
+from sqlalchemy import Numeric, Enum as SqlaEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from app.db.base import Base
 
+if TYPE_CHECKING:
+    from app.models.payments import Payment
+
 class OrderStatus(str, enum.Enum):
-    UNPAYED = "unpayied"
+    UNPAID = "unpaid"
+    PARTIALLY_PAID = "partially_paid"
     PAID = "paid"
-    REFUNDED = "refunded"
+    ARCHIVED = "archived"
 
 class Order(Base):
     __tablename__ = "orders"
 
-    id = Column(Integer, primary_key=True, index=True)
-    total_amount = Column(Numeric(10, 2), nullable=False)
-    status = Column(Enum(OrderStatus), default=OrderStatus.UNPAYED, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    total_amount: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    status: Mapped[OrderStatus] = mapped_column(SqlaEnum(OrderStatus), default=OrderStatus.UNPAID, nullable=False)
 
-
+    payments: Mapped[List["Payment"]] = relationship("Payment", back_populates="order")
